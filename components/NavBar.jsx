07c2dc233e2e2b0aa6774258fd7bcadda0f16e81
Kar Rakib <rakibkazi2222@gmail.com>
2023-08-21 17:29:38 +0600

    nav

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,29 +3,18 @@ import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <div>
-      <div className="Navbar bg-base-100">
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">CRUD-WEB </a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li><a>Link</a></li>
-            <li>
-              <details>
-                <summary>
-                  Parent
-                </summary>
-                <ul className="p-2 bg-base-100">
-                  <li><Link href={'/'}> Home  </Link></li>
-                  <li><Link href={'/addpost'}>Add Post </Link></li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><Link href={'/'}> Home</Link> </li>
+      <li><Link href={'/addpost'}>Add Post </Link> </li>
+      
+    </ul>
+  </div>
+</div>
   );
 };
 
